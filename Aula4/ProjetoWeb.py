25701a47ada204a1d3621e53a5a112b11eb43fc3
artur.pires <artur.pires@fiscaltech.com.br>
2025-03-26 20:10:53 -0300

    alteração da edição de imagem

--- Aula4/ProjetoWeb.py
+++ Aula4/ProjetoWeb.py
@@ -24,8 +24,25 @@ def upload():
 
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
     file.save(file_path)
+
+    showImage(file_path)
+
     return f"Imagem salva em {file_path}", 200
 
+def showImage(imagem):
+    img = cv2.imread(imagem)
+    convertImage(img)
+    blur_img = applyBlur(img)
+
+    cv2.imshow("imagem", blur_img)
+    cv2.waitKey(0)  # aguarda alguma tecla ser pressionada para fechar a imagem
+    cv2.destroyAllWindows()
+
+def convertImage(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
+
+def applyBlur(image):
+    return cv2.blur(image[:,:,0],(15,15),0)
 
 if __name__ == '__main__':
     app.run(debug=True)
