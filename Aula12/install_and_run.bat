dff4c6f22dec4f6c1ca04484caf3baf4f42ee463
ArtPires <apiresamador@gmail.com>
2025-05-28 11:41:33 -0300

    adicionando clone dos modelos no script de inicialização

--- Aula12/install_and_run.bat
+++ Aula12/install_and_run.bat
@@ -1,30 +1,30 @@
 @echo off
-echo ===============================
-echo  Criando ambiente virtual...
-echo ===============================
+echo [1/5] Criando ambiente virtual...
 python -m venv venv
 
-echo ===============================
-echo  Ativando ambiente virtual...
-echo ===============================
-call venv\Scripts\activate.bat
+echo [2/5] Ativando ambiente virtual...
+call venv\Scripts\activate
 
-echo ===============================
-echo  Atualizando pip...
-echo ===============================
-python -m pip install --upgrade pip
+echo [3/5] Atualizando pip e instalando dependências...
+pip install --upgrade pip
+pip install opencv-python numpy requests
 
-echo ===============================
-echo  Instalando dependências...
-echo ===============================
-pip install -r requirements.txt
+echo [4/5] Baixando modelos Caffe do OpenPose...
 
-echo ===============================
-echo  Iniciando o programa...
-echo ===============================
+if not exist "models" mkdir models
+cd models
+
+:: Baixar pose_deploy_linevec.prototxt
+echo Baixando prototxt...
+curl -L -o pose_deploy_linevec.prototxt https://raw.githubusercontent.com/CMU-Perceptual-Computing-Lab/openpose/master/models/pose/body_25/pose_deploy.prototxt
+
+:: Baixar pose_iter_584000.caffemodel
+echo Baixando caffemodel...
+curl -L -o pose_iter_584000.caffemodel https://github.com/CMU-Perceptual-Computing-Lab/openpose/blob/master/models/pose/body_25/pose_iter_584000.caffemodel?raw=true
+
+cd ..
+
+echo [5/5] Iniciando o programa...
 python main.py
 
-echo ===============================
-echo  Encerrado.
-echo ===============================
 pause
